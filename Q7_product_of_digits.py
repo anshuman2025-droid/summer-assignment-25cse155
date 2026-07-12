06432847b45d2e61b91f63fbeb3da29fa5d1d16e
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:40:59 +0530

    Delete Q7_product_of_digits.py

--- Q7_product_of_digits.py
+++
@@ -1,9 +0,0 @@
-# Q7 - Find product of digits
-def product_of_digits(n):
-    result = 1
-    for d in str(abs(n)):
-        result *= int(d)
-    return result
-
-n = int(input("Enter number: "))
-print(f"Product of digits = {product_of_digits(n)}")

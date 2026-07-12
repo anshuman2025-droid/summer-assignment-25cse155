6f7dee534290573b013957b2988b58fad1dff992
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:39:05 +0530

    Delete Q15_armstrong.py

--- Q15_armstrong.py
+++
@@ -1,8 +0,0 @@
-# Q15 - Check Armstrong number
-def is_armstrong(n):
-    digits = str(n)
-    power = len(digits)
-    return n == sum(int(d) ** power for d in digits)
-
-n = int(input("Enter number: "))
-print("Armstrong" if is_armstrong(n) else "Not Armstrong")

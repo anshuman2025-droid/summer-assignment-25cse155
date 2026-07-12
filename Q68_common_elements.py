67a6bd566974291b2795b8ce7b8218cfa1d0e02f
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:44:46 +0530

    Delete Q68_common_elements.py

--- Q68_common_elements.py
+++
@@ -1,4 +0,0 @@
-# Q68 - Find common elements
-arr1 = list(map(int, input("Enter first array: ").split()))
-arr2 = list(map(int, input("Enter second array: ").split()))
-print("Common elements:", [x for x in arr1 if x in arr2])

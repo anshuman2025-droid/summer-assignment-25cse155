6a5ae8784d0b4c259af0b859428102fb96508a45
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:52:42 +0530

    Delete Q52_even_odd_count.py

--- Q52_even_odd_count.py
+++
@@ -1,5 +0,0 @@
-# Q52 - Count even and odd elements
-arr = list(map(int, input("Enter elements: ").split()))
-even = sum(1 for x in arr if x % 2 == 0)
-odd = len(arr) - even
-print(f"Even count = {even}, Odd count = {odd}")

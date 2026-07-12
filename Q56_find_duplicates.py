418c0e37f99fc8b608a7fbf5dd08c3dc0fc5b765
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:51:57 +0530

    Delete Q56_find_duplicates.py

--- Q56_find_duplicates.py
+++
@@ -1,9 +0,0 @@
-# Q56 - Find duplicates in array
-arr = list(map(int, input("Enter elements: ").split()))
-seen = set()
-duplicates = set()
-for x in arr:
-    if x in seen:
-        duplicates.add(x)
-    seen.add(x)
-print(f"Duplicates = {list(duplicates)}")

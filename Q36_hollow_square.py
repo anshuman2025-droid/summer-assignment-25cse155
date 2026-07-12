f0a7ae9b32bd40a15ad2b68a95970e4dba03c9c7
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:50:17 +0530

    Delete Q36_hollow_square.py

--- Q36_hollow_square.py
+++
@@ -1,7 +0,0 @@
-# Q36 - Print hollow square pattern
-n = int(input("Enter size: "))
-for i in range(n):
-    if i == 0 or i == n - 1:
-        print('*' * n)
-    else:
-        print('*' + ' ' * (n - 2) + '*')

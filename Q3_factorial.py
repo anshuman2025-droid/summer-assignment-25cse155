b0972bd8ae686c7f9d7d7aac02d618865a472b91
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:50:58 +0530

    Delete Q3_factorial.py

--- Q3_factorial.py
+++
@@ -1,9 +0,0 @@
-# Q3 - Find factorial of a number
-def factorial(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
-
-n = int(input("Enter number: "))
-print(f"Factorial of {n} = {factorial(n)}")

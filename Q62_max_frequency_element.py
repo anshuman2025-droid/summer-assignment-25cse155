7c4523b0ce146892b0569f26aeb841584a6be503
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:46:04 +0530

    Delete Q62_max_frequency_element.py

--- Q62_max_frequency_element.py
+++
@@ -1,5 +0,0 @@
-# Q62 - Find maximum frequency element
-from collections import Counter
-arr = list(map(int, input("Enter elements: ").split()))
-c = Counter(arr)
-print(f"Max frequency element = {c.most_common(1)[0][0]}")

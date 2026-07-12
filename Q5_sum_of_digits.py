d3a50bcf79ac0a6cbb9d2e21daf92fb8b520fdd8
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:51:07 +0530

    Delete Q5_sum_of_digits.py

--- Q5_sum_of_digits.py
+++
@@ -1,6 +0,0 @@
-# Q5 - Find sum of digits of a number
-def sum_of_digits(n):
-    return sum(int(d) for d in str(abs(n)))
-
-n = int(input("Enter number: "))
-print(f"Sum of digits = {sum_of_digits(n)}")

ded5bb1ca673c9dd48846de968dff7aa7622e63e
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:41:40 +0530

    Delete Q1_sum_natural.py

--- Q1_sum_natural.py
+++
@@ -1,6 +0,0 @@
-# Q1 - Calculate sum of first N natural numbers
-def sum_natural(n):
-    return n * (n + 1) // 2
-
-n = int(input("Enter N: "))
-print(f"Sum of first {n} natural numbers = {sum_natural(n)}")

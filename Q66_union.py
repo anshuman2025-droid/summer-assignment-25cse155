9d41ef5d894ee6a1366260cd7fbe69ca559fb094
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:45:10 +0530

    Delete Q66_union.py

--- Q66_union.py
+++
@@ -1,4 +0,0 @@
-# Q66 - Union of arrays
-arr1 = list(map(int, input("Enter first array: ").split()))
-arr2 = list(map(int, input("Enter second array: ").split()))
-print("Union:", list(set(arr1) | set(arr2)))

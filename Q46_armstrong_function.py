881d58c1d61fbd45781ab07e6e82b2a0d51b89f4
Anshuman2025 <anshuman.2025@glbajajgroup.org>
2026-07-12 14:53:59 +0530

    Delete Q46_armstrong_function.py

--- Q46_armstrong_function.py
+++
@@ -1,7 +0,0 @@
-# Q46 - Function for Armstrong
-def is_armstrong(n):
-    digits = str(n)
-    return n == sum(int(d) ** len(digits) for d in digits)
-
-n = int(input("Enter number: "))
-print("Armstrong" if is_armstrong(n) else "Not Armstrong")
